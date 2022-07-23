2ae74ec89c0a5a460681c7bf96a7e18f0b80a267
aurora-721 <aurora.makovac@gmail.com>
2022-07-23 21:34:34 +0200

    adding header

--- frontend_final/quest-frrontend/pages/_app.js
+++ frontend_final/quest-frrontend/pages/_app.js
@@ -15,7 +15,6 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-import Footer from './../components/Footer';
 
 import "./../public/assets/css/animate.min.css";
 import "./../public/assets/css/aos.css";
@@ -57,14 +56,123 @@ function MyApp({ Component, pageProps }) {
 
       <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <header>
-          <Link href="/leaderboard">Leaderboard</Link> |
-          <Link href="/quests">Quests</Link>
-          <ConnectButton />
-        
+        <header class="third-header-bg home-five-header">
+            <div class="bg"></div>
+            <div class="container custom-container">
+                <div class="header-top-area t-header-top-area d-none d-lg-block">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="header-top-social">
+                                <ul>
+                                    <li>Follow</li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="header-top-login">
+                                <ul>
+                                <ConnectButton />
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="sticky-header">
+                <div class="container custom-container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="main-menu menu-style-two">
+                                <nav>
+                                    <div class="logo d-block d-lg-none">
+                                        <a href="index-5.html"><img src="./assets/img/logo/logo.png" alt="Logo"/></a>
+                                    </div>
+                                    <div class="navbar-wrap d-none d-lg-flex">
+                                        <ul class="left">
+                                          <li><Link href="/">Quests</Link> </li>
+                                          <li><Link href="/leaderboard">leaderboard</Link></li>
+                                            <li><a href="community.html">Community</a></li>
+                                        </ul>
+                                        <div class="logo">
+                                            <a href="index-3.html"><img src="./assets/img/logo/h5_logo.png" alt="Logo"/></a>
+                                        </div>
+                                        <ul class="right">
+                                            <li><a href="#">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">News Page</a></li>
+                                                    <li><a href="blog-details.html">News Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">contact</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="header-action">
+                                        <ul>
+                                            <li class="header-search"><a href="#" data-toggle="modal" data-target="#search-modal"><i class="fas fa-search"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="mobile-menu-wrap d-block d-lg-none">
+                                <nav>
+                                    <div id="mobile-menu" class="navbar-wrap">
+                                        <ul>
+                                            <li class="show"><a href="#">Home</a>
+                                                <ul class="submenu">
+                                                    <li><a href="index-2.html">Home One</a></li>
+                                                    <li><a href="index-3.html">Home Two</a></li>
+                                                    <li><a href="index-4.html">Home Three</a></li>
+                                                    <li><a href="index.html">Home Four</a></li>
+                                                    <li><a href="index-5.html">Home Five</a></li>
+                                                    <li><a href="index-6.html">Home Six</a></li>
+                                                    <li><a href="index-7.html">Home Seven</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="about-us.html" >About</a></li>
+                                            <li><a href="game-overview.html">Overview</a></li>
+                                            <li><a href="community.html">Community</a></li>
+                                            <li><a href="#">Pages</a>
+                                                <ul class="submenu">
+                                                    <li><a href="upcoming-games.html">Upcoming Games</a></li>
+                                                    <li><a href="game-single.html">Game Single</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="shop.html">Store</a></li>
+                                            <li><a href="#">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">News Page</a></li>
+                                                    <li><a href="blog-details.html">News Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">contact</a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="mobile-menu"></div>
+                        </div>
+                        <div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <form>
+                                        <input type="text" placeholder="Search here..."/>
+                                        <button><i class="fa fa-search"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom-bg"></div>
         </header>
+        
+
         <Component {...pageProps} />
-        <Footer />
 
       </RainbowKitProvider>
     </WagmiConfig>
--- frontend_final/quest-frrontend/pages/index.js
+++ frontend_final/quest-frrontend/pages/index.js
@@ -1,19 +1,54 @@
-
-
-
 /*import contract from "./contracts/NumberBox.json";
 
 const contractAddress = "0xFDA9C8A3d94A9786d9639Dca33b92604802ba6e0";
 const abi = contract.abi;*/
+import React from 'react';
+import QuestCardComp from '../components/QuestCardComp';
+import getInfosForCard from '../utils/data/getInfosFromCard.js';
+
+import Footer from './../components/Footer';
 
 
 export default function Home() {
 
-  
-
-
-
   return (
-    <p>test</p>
+    <>
+    <section className="trending-gamepay-area">
+        <div className="container">
+            <div className="row align-items-center mb-30">
+                <div className="col-sm-6">
+                    <div className="hf-section-title">
+                        <h4 className="title">Trending quests</h4>
+                    </div>
+                </div>
+                <div className="col-sm-6 d-none d-sm-block">
+  
+                </div>
+            </div>
+  
+            <div className="row">
+                    {
+                        getInfosForCard.map((item, key) => {
+                            return (
+                            <QuestCardComp 
+                                key={key}
+                                id={item.id} 
+                                description={item.description} 
+                                title={item.title}
+                                reward={item.reward}
+                                gameName={item.game_name}
+                                img={item.image}
+                            />
+                            )
+                            
+                        })
+                    }
+              
+            </div>
+        </div>
+    </section>
+    <Footer />
+
+    </>  
   )
 }
