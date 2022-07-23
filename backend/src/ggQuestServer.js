0742bc264f12ecbd4cd54f2bd9c94e8956b11dee
H0tmilk <k.chlt@protonmail.com>
2022-07-23 21:48:01 +0200

    Add CORS support

--- backend/src/ggQuestServer.js
+++ backend/src/ggQuestServer.js
@@ -1,6 +1,8 @@
 const dataProvider = require('./dataProvider.js')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors)
 const port = 8080
 
 app.get('/', (req, res) => {
