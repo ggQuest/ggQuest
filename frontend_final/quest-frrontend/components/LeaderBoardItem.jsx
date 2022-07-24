f8a260932d64827b790b5bff194fbebb8e3c957b
aurora-721 <aurora.makovac@gmail.com>
2022-07-24 10:31:14 +0200

    adding styling

--- frontend_final/quest-frrontend/components/LeaderBoardItem.jsx
+++ frontend_final/quest-frrontend/components/LeaderBoardItem.jsx
@@ -5,10 +5,10 @@ function LeaderBoardItem({score, address}) {
   return (
     <>
      <div className="row" style={{background: '#0e0f15', padding: 15, margin: 5}}>
-                <div style={{paddingRight: 10}}>Player : {address} </div>
-                <div style={{background: 'red', width: 4}}></div>
+        <div style={{paddingRight: 10}}>Player : {address} </div>
+        <div style={{background: 'red', width: 4}}></div>
 
-                <div style={{paddingLeft: 10}} className="score">Score :  {score} </div>
+        <div style={{paddingLeft: 10}} className="score">Score :  {score} </div>
       </div>
    
     </>
--- frontend_final/quest-frrontend/pages/stats.jsx
+++ frontend_final/quest-frrontend/pages/stats.jsx
@@ -66,6 +66,7 @@ function StatsPage() {
         </div>
        
     </section>
+    
     </>
   )
 }
