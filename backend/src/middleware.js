7f8ac7240120700fbcbd859f843bb4a9b64ec67f
codingsh <codingsh@pm.me>
2022-12-07 03:54:11 +0000

    chore(api): update middleware

--- backend/src/middleware.js
+++ backend/src/middleware.js
@@ -3,13 +3,24 @@ const tokenSecret = "afp:pcvq3rZ=swvwhdwh!pcigyj"
 
 exports.tokenSecret = tokenSecret
 exports.verify = (req, res, next) => {
-    const token = req.headers.authorization
-    if (!token) res.status(403).json({error: "Please provide a API token to use this endpoint."})
-    else {
-        jwt.verify(token.split(" ")[1], tokenSecret, (err, value) => {
-            if (err) res.status(500).json({error: 'Failed to authenticate token'})
-            req.user = value.data
-            next()
-        })
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(403).json({
+        error: "Please provide a API token to use this endpoint.",
+      });
     }
-}
+  
+    try {
+      const decoded = jwt.verify(token.split(" ")[1], tokenSecret);
+      req.user = decoded.data;
+      return next();
+    } catch (err) {
+      return res.status(500).json({ error: "Failed to authenticate token" });
+    }
+  };
+
+
+
+
+
+  
