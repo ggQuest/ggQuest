2ba31389cdee4b4e872fea67b64417b8c47df2f8
codingsh <codingsh@pm.me>
2022-12-08 13:01:51 +0000

    comeback old middleware

--- backend/src/middleware.js
+++ backend/src/middleware.js
@@ -6,30 +6,14 @@ if (!process.env.TOKEN_SECRET) {
 }
 const tokenSecret = process.env.TOKEN_SECRET
 
-const verifyToken = (token) => {
-  try {
-    const decoded = jwt.verify(token.split(" ")[1], tokenSecret);
-    return decoded.data;
-  } catch (err) {
-    return res.status(500).json({ error: "Failed to authenticate token" });
-  }
-};
-
 exports.verify = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(403).json({
-      error: "Please provide a API token to use this endpoint.",
-    });
+  const token = req.headers.authorization
+  if (!token) res.status(403).json({error: "Please provide a API token to use this endpoint."})
+  else {
+      jwt.verify(token.split(" ")[1], tokenSecret, (err, value) => {
+          if (err) res.status(500).json({error: 'Failed to authenticate token'})
+          req.user = value.data
+          next()
+      })
   }
-
-  if (!token.startsWith("Bearer ") || token.length <= "Bearer ".length) {
-    return res.status(400).json({ error: "Invalid token format" });
-  }
-
-  const user = verifyToken(token);
-  if (user) {
-    req.user = user;
-    return next();
-  }
-};
+}
